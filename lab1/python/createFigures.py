031787e665691db0ff7e5ca0fb206949f9647ce0
LehrChristoph <christoph.lehr@gmx.at>
2020-12-05 11:56:24 +0100

    added generation of mtbu and fr figures

--- lab1/python/createFigures.py
+++ lab1/python/createFigures.py
@@ -1,6 +1,6 @@
-import matplotlib
-import numpy as np 
 import os
+import numpy as np 
+import matplotlib.pyplot as plt
 from pprint import pprint
 
 def import_data(import_folder):
@@ -27,22 +27,28 @@ def do_calculations(datasets):
         T_clk = 1/f_clk
         dataset["settings"]['ps_mult'] = 104.16 * 10**-12 #104.16 pico seconds
         
-        calculate_tbu_mtbf(dataset)
+        calculate_tbu_mtbu(dataset)
         calculate_t_res(dataset)
-        [tau,offset] = calculate_T0_tau(dataset["data"]["t_res"], dataset["data"]["mtbf"])
+        [tau,offset] = calculate_T0_tau(dataset["data"]["t_res"], dataset["data"]["mtbu"])
         T0 = (1/(offset*f_clk*lambda_dat))
         dataset["data"]["tau"] = tau
         dataset["data"]["T0"] = T0
     # end for
 #end def
 
-def calculate_tbu_mtbf(dataset):
+def calculate_tbu_mtbu(dataset):
     datapoint_cnt = dataset["settings"]['last_data_point'] +1
-    dataset["data"]["mtbf"] = [None]*datapoint_cnt
-    dataset["data"]["mtbf_0to0"] = [None]*datapoint_cnt
-    dataset["data"]["mtbf_0to1"] = [None]*datapoint_cnt
-    dataset["data"]["mtbf_1to1"] = [None]*datapoint_cnt
-    dataset["data"]["mtbf_1to0"] = [None]*datapoint_cnt
+    dataset["data"]["mtbu"] = [None]*datapoint_cnt
+    dataset["data"]["mtbu_0to0"] = [None]*datapoint_cnt
+    dataset["data"]["mtbu_0to1"] = [None]*datapoint_cnt
+    dataset["data"]["mtbu_1to1"] = [None]*datapoint_cnt
+    dataset["data"]["mtbu_1to0"] = [None]*datapoint_cnt
+
+    dataset["data"]["fr"] = [None]*datapoint_cnt
+    dataset["data"]["fr_0to0"] = [None]*datapoint_cnt
+    dataset["data"]["fr_0to1"] = [None]*datapoint_cnt
+    dataset["data"]["fr_1to1"] = [None]*datapoint_cnt
+    dataset["data"]["fr_1to0"] = [None]*datapoint_cnt
 
     dataset["data"]["tbu"] = [None]*datapoint_cnt
     dataset["data"]["tbu_0to0"] = [None]*datapoint_cnt
@@ -57,7 +63,8 @@ def calculate_tbu_mtbf(dataset):
             tbu.append(run_upsets[index+1] - run_upsets[index])
         # end for
         dataset["data"]["tbu"][datapoint_id] = tbu
-        dataset["data"]["mtbf"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["mtbu"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["fr"][datapoint_id] = 1/np.mean(tbu)
 
         run_upsets = dataset["data"]["upset_times_0to0"][datapoint_id]
         tbu = []
@@ -65,7 +72,8 @@ def calculate_tbu_mtbf(dataset):
             tbu.append(run_upsets[index+1] - run_upsets[index])
         # end for
         dataset["data"]["tbu_0to0"][datapoint_id] = tbu
-        dataset["data"]["mtbf_0to0"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["mtbu_0to0"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["fr_0to0"][datapoint_id] = 1/np.mean(tbu)
         
         run_upsets = dataset["data"]["upset_times_0to1"][datapoint_id]
         tbu = []
@@ -73,7 +81,8 @@ def calculate_tbu_mtbf(dataset):
             tbu.append(run_upsets[index+1] - run_upsets[index])
         # end for
         dataset["data"]["tbu_0to1"][datapoint_id] = tbu
-        dataset["data"]["mtbf_0to1"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["mtbu_0to1"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["fr_0to1"][datapoint_id] = 1/np.mean(tbu)
 
         run_upsets = dataset["data"]["upset_times_1to1"][datapoint_id]
         tbu = []
@@ -81,7 +90,8 @@ def calculate_tbu_mtbf(dataset):
             tbu.append(run_upsets[index+1] - run_upsets[index])
         # end for
         dataset["data"]["tbu_1to1"][datapoint_id] = tbu
-        dataset["data"]["mtbf_1to1"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["mtbu_1to1"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["fr_1to1"][datapoint_id] = 1/np.mean(tbu)
 
         run_upsets = dataset["data"]["upset_times_1to0"][datapoint_id]
         tbu = []
@@ -89,7 +99,8 @@ def calculate_tbu_mtbf(dataset):
             tbu.append(run_upsets[index+1] - run_upsets[index])
         # end for
         dataset["data"]["tbu_1to0"][datapoint_id] = tbu
-        dataset["data"]["mtbf_1to0"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["mtbu_1to0"][datapoint_id] = np.mean(tbu)
+        dataset["data"]["fr_1to0"][datapoint_id] = 1/np.mean(tbu)
     # end for
 # end def
 
@@ -136,12 +147,93 @@ def store_data(filepath, datasets):
     # end def
 # end def
 
-def generate_figures():
-    pass
+def generate_figures(datasets, export_folder):
+    for dataset_name in datasets:
+        dataset = datasets[dataset_name]
+        folder = os.path.join(export_folder, dataset_name)
+        plot_mtbu(dataset,folder)
+        plot_fr(dataset,folder)
+        plot_tbu_distribution(dataset,folder)
+    # end def
 # end def
 
+def plot_mtbu(dataset, export_folder):
+    t_res = np.array(dataset["data"]["t_res"])
+
+    mtbu = np.array(dataset["data"]["mtbu"])
+    mtbu_mask = np.isfinite(mtbu.astype(np.double))
+
+    mtbu_0to0 = np.array(dataset["data"]["mtbu_0to0"])
+    mtbu_0to0_mask = np.isfinite(mtbu_0to0.astype(np.double))
+
+    mtbu_0to1 = np.array(dataset["data"]["mtbu_0to1"])
+    mtbu_0to1_mask = np.isfinite(mtbu_0to1.astype(np.double))
+
+    mtbu_1to1 = np.array(dataset["data"]["mtbu_1to1"])
+    mtbu_1to1_mask = np.isfinite(mtbu_1to1.astype(np.double))
+
+    mtbu_1to0 = np.array(dataset["data"]["mtbu_1to0"])
+    mtbu_1to0_mask = np.isfinite(mtbu_1to0.astype(np.double))
+
+    plt.plot(t_res[mtbu_mask], mtbu[mtbu_mask])
+    plt.plot(t_res[mtbu_0to0_mask], mtbu_0to0[mtbu_0to0_mask])
+    plt.plot(t_res[mtbu_0to1_mask], mtbu_0to1[mtbu_0to1_mask])
+    plt.plot(t_res[mtbu_1to1_mask], mtbu_1to1[mtbu_1to1_mask])
+    plt.plot(t_res[mtbu_1to0_mask], mtbu_1to0[mtbu_1to0_mask])
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.title('MTBU')
+
+    if not os.path.exists(export_folder):
+        os.makedirs(export_folder)
+    # end if
+
+    filepath = os.path.join(export_folder, 'MTBU.jpeg')
+    plt.savefig(filepath)
+    plt.close()
+# end def
+
+def plot_fr(dataset, export_folder):
+    t_res = np.array(dataset["data"]["t_res"])
+
+    fr = np.array(dataset["data"]["fr"])
+    fr_mask = np.isfinite(fr.astype(np.double))
+
+    fr_0to0 = np.array(dataset["data"]["fr_0to0"])
+    fr_0to0_mask = np.isfinite(fr_0to0.astype(np.double))
+
+    fr_0to1 = np.array(dataset["data"]["fr_0to1"])
+    fr_0to1_mask = np.isfinite(fr_0to1.astype(np.double))
+
+    fr_1to1 = np.array(dataset["data"]["fr_1to1"])
+    fr_1to1_mask = np.isfinite(fr_1to1.astype(np.double))
+
+    fr_1to0 = np.array(dataset["data"]["fr_1to0"])
+    fr_1to0_mask = np.isfinite(fr_1to0.astype(np.double))
+
+    plt.plot(t_res[fr_mask], fr[fr_mask])
+    plt.plot(t_res[fr_0to0_mask], fr_0to0[fr_0to0_mask])
+    plt.plot(t_res[fr_0to1_mask], fr_0to1[fr_0to1_mask])
+    plt.plot(t_res[fr_1to1_mask], fr_1to1[fr_1to1_mask])
+    plt.plot(t_res[fr_1to0_mask], fr_1to0[fr_1to0_mask])
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.title('Failure Rate')
+
+    if not os.path.exists(export_folder):
+        os.makedirs(export_folder)
+    # end if
+    filepath = os.path.join(export_folder, 'failure_rate.jpeg')
+    plt.savefig(filepath)
+    plt.close()
+# end def
+
+def plot_tbu_distribution(dataset, export_folder):
+    pass
+# end def
 
 if __name__ == "__main__":
     data = import_data("./measurements")
     do_calculations(data)
     store_data("./figure_data.np",data)
+    generate_figures(data, "./figures")
