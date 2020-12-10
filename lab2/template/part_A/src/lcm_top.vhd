6a6292e7ce38a5d501c13f7d5ebc40db7e76b2f6
LehrChristoph <christoph.lehr@gmx.at>
2020-12-10 10:18:40 +0100

    implemented top entity

--- lab2/template/part_A/src/lcm_top.vhd
+++ lab2/template/part_A/src/lcm_top.vhd
@@ -6,25 +6,55 @@ use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
 entity LCM_top is
-    port(
-	clk: in std_logic;
-        res_n: in std_logic;
-        A, B : in std_logic_vector(3 downto 0);
-        A_deb, B_deb : out std_logic_vector(3 downto 0);
-        req_AB: in std_logic;
-        ack_AB: out std_logic;
-        result: out std_logic_vector(7 downto 0);
-        req_result: out std_logic;
-        ack_result: in std_logic
-         );
+   port(
+		clk: in std_logic;
+		res_n: in std_logic;
+		A, B : in std_logic_vector(3 downto 0);
+		A_deb, B_deb : out std_logic_vector(3 downto 0);
+		req_AB: in std_logic;
+		ack_AB: out std_logic;
+		result: out std_logic_vector(7 downto 0);
+		req_result: out std_logic;
+		ack_result: in std_logic
+	);
 end LCM_top;
 
 architecture STRUCTURE of LCM_top is
 
+constant DATA_WIDTH : Integer := 8;
+
+component lcm is	
+	generic ( DATA_WIDTH : natural);
+	port (
+		AB : in std_logic_vector ( DATA_WIDTH-1 downto 0 );
+		RESULT : out std_logic_vector ( DATA_WIDTH-1 downto 0 );
+		rst : in std_logic;
+		i_req:  in std_logic;
+		i_ack:  out std_logic;
+		o_req: out std_logic;
+		o_ack: in std_logic
+	);
+end component lcm;
+	
+	signal AB, result_tmp : std_logic_vector(DATA_WIDTH-1 downto 0);
 
 begin
-
-    A_deb <= A;
-    B_deb <= B;
-
+	
+lcm_calc: component lcm
+	generic map ( DATA_WIDTH => DATA_WIDTH)
+	port map(
+		AB => AB,
+		RESULT => result_tmp,
+		rst => res_n,
+		i_req => req_AB,
+		i_ack => ack_AB,
+		o_req => req_result,
+		o_ack => ack_result
+	);
+	
+	AB(DATA_WIDTH   -1 downto DATA_WIDTH/2) <= A;
+	AB(DATA_WIDTH/2 -1 downto            0) <= B;
+	A_deb <= result_tmp(DATA_WIDTH   -1 downto DATA_WIDTH/2);
+	B_deb <= result_tmp(DATA_WIDTH/2 -1 downto            0);
+	result <= result_tmp;
 end STRUCTURE;
