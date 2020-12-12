7e641209b2e3b750f0ccf8dd8b31fec5fbd1dcca
Julian Spitzer <spitzer.julian@gmail.com>
2020-12-12 14:22:08 +0100

    Update comparison plots

--- lab1/python/createFigures.py
+++ lab1/python/createFigures.py
@@ -5,7 +5,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 from pprint import pprint
 matplotlib.use("Agg")
-
+excessMeasurements = 6
 
 def import_data(import_folder):
     data = {}
@@ -16,8 +16,17 @@ def import_data(import_folder):
             with open(filepath, "rb") as fd:
                 data_file = np.load(fd, allow_pickle=True)
                 data[file_name[:-3]] = data_file[0]["results"][0]
-                data[file_name[:-3]]["settings"]["last_data_point"] = data_file[0]["state"]["current_data_point"]
-                data[file_name[:-3]]['data']['ps_values'] = data[file_name[:-3]]['data']['ps_values']-6
+
+                d = data[file_name[:-3]]
+
+                d["settings"]["last_data_point"] = data_file[0]["state"]["current_data_point"] - excessMeasurements
+                d['data']['ps_values'] = d['data']['ps_values'] - excessMeasurements
+
+                for key in d['data'].keys():
+                    d['data'][key] = d['data'][key][excessMeasurements:]
+
+#                import pdb; pdb.set_trace()
+
             # end with
         # end for
     # end for
@@ -44,7 +53,7 @@ def do_calculations(datasets):
         t_res_glitch_min = 0
         t_res_0 =0
         for i in range(len(dataset["data"]["t_res"])):
-            if(dataset["data"]["t_res"][i] > 200 * 10**-12 and t_res_min == 0):
+            if(dataset["data"]["t_res"][i] > 300 * 10**-12 and t_res_min == 0):
                 t_res_min = i
             # end if
             if(dataset["data"]["t_res"][i] < 700 * 10**-12):
@@ -133,12 +142,13 @@ def calculate_T0_tau(x, y):
             continue
         # end if
         x_poly_fit.append(x[i])
-        y_poly_fit.append(y[i])
+        y_poly_fit.append(np.log(y[i]))
     # end for
 
     if(len(x_poly_fit)>0 ):
         (tau, T0 ) = np.polyfit( x_poly_fit, y_poly_fit, 1 )
         tau = 1/tau
+        T0 = np.exp(T0)
     else:
         tau = np.nan
         T0 = np.nan
@@ -392,22 +402,33 @@ def plot_mtbu_comparision(datasets,export_folder, plot_dataset=[]):
         return
     # end if
 
+    plt.figure(figsize=(12,7))
+
+    plotColors = ['maroon', 'darkcyan', 'mediumorchid', 'coral']
+
     for dataset_name in datasets:
         if(dataset_name not in plot_dataset):
             continue
+        else:
+            color = plotColors[0]
+            plotColors.remove(color)
         # end if
         dataset = datasets[dataset_name]["data"]
         mtbu = np.array(dataset["mtbu"])
         t_res = np.array(dataset["t_res"])*10**12 # Plot in ps
 
+        T0_s = None
+        tau_s = None
+
         mtbu_mask = np.isfinite(mtbu.astype(np.double))
-        plt.plot(t_res[mtbu_mask], mtbu[mtbu_mask])
-        legend_str = dataset_name.replace("_", " ")+"%"
-        legend_str+= "\n$\\tau_M:{:.2e}s, T0_M:{:.2e}s$".format(dataset["tau"], dataset["T0"])
+        plt.plot(t_res[mtbu_mask], mtbu[mtbu_mask], color=color)
+        legend_str = dataset_name.capitalize().replace("_", " ")+"%"
         if(not np.isnan(dataset["tau_1to1"])):
-            legend_str+= "\n$\\tau_S:{:.2e}s, T0_S:{:.2e}s$".format(dataset["tau_1to1"], dataset["T0_1to1"])
+            T0_s = dataset["T0_1to1"]
+            tau_s = dataset["tau_1to1"]
         elif(not np.isnan(dataset["tau_0to0"])):
-            legend_str+= "\n$\\tau_S:{:.2e}s, T0_S:{:.2e}s$".format(dataset["tau_0to0"], dataset["T0_0to0"])
+            T0_s = dataset["T0_0to0"]
+            tau_s = dataset["tau_0to0"]
         # end if
         legend.append(legend_str)
 
@@ -420,12 +441,23 @@ def plot_mtbu_comparision(datasets,export_folder, plot_dataset=[]):
 
         approxTr = []
         approxMTBU = []
-        for tr in [t_res[0], t_res[-1]]:
+        for tr in [-200, 900]:
             approxTr.append(tr)
             approxMTBU.append(MTBU(tr/10**12))
 
-        plt.plot(approxTr, approxMTBU)
-        legend.append("Approx")
+        plt.plot(approxTr, approxMTBU, '--', color=color)
+        legend.append(f"$\\tau_M:{tau*10**12:.02f}ps, T0_M:{T0*10**12:.02f}ps$")
+
+        if T0_s:
+            MTBU = lambda tres : 1/(lambda_dat*f_clk*T0_s)*np.exp(tres/tau_s)
+            approxTr = []
+            approxMTBU = []
+            for tr in [600, 1200]:
+                approxTr.append(tr)
+                approxMTBU.append(MTBU(tr/10**12))
+
+            plt.plot(approxTr, approxMTBU, ':', color=color)
+            legend.append(f"$\\tau_S:{tau_s*10**12:.02f}ps, T0_S:{T0_s*10**18:.02f}as$")
 
 
         lastTr = t_res[np.isfinite(np.array(dataset["mtbu"]))][-1]
@@ -447,6 +479,8 @@ def plot_mtbu_comparision(datasets,export_folder, plot_dataset=[]):
     plt.ylabel('MTBU (s)')
     plt.xlabel('Resolution time (ps)')
 
+    plt.ylim([10**(-9), 10**5])
+
     plt.yticks([10**i for i in range(-8, 5)])
     plt.xticks(range(math.floor(int(t_res[0])/200)*200, int(lastTr)+50, 200) )
 
@@ -466,6 +500,7 @@ def plot_fr_comparision(datasets,export_folder, plot_dataset=[]):
     legend = []
     max_Tr= -np.inf
     min_Tr= np.inf
+    plt.figure(figsize=(12,7))
 
     if(len(plot_dataset) == 0):
         return
@@ -482,13 +517,13 @@ def plot_fr_comparision(datasets,export_folder, plot_dataset=[]):
         mtbu_mask = np.isfinite(mtbu.astype(np.double))
         plt.plot(t_res[mtbu_mask], mtbu[mtbu_mask])
 
-        legend_str = dataset_name.replace("_", " ")
-        legend_str+= "\n$\\tau_M:{:.2e}s, T0_M:{:.2e}s$".format(dataset["tau"], dataset["T0"])
-        if(not np.isnan(dataset["tau_1to1"])):
-            legend_str+= "\n$\\tau_S:{:.2e}s, T0_S:{:.2e}s$".format(dataset["tau_1to1"], dataset["T0_1to1"])
-        elif(not np.isnan(dataset["tau_0to0"])):
-            legend_str+= "\n$\\tau_S:{:.2e}s, T0_S:{:.2e}s$".format(dataset["tau_0to0"], dataset["T0_0to0"])
-        # end if
+        legend_str = dataset_name.capitalize().replace("_", " ")+"%"
+#        legend_str+= "\n$\\tau_M:{:.2e}s, T0_M:{:.2e}s$".format(dataset["tau"], dataset["T0"])
+#        if(not np.isnan(dataset["tau_1to1"])):
+#            legend_str+= "\n$\\tau_S:{:.2e}s, T0_S:{:.2e}s$".format(dataset["tau_1to1"], dataset["T0_1to1"])
+#        elif(not np.isnan(dataset["tau_0to0"])):
+#            legend_str+= "\n$\\tau_S:{:.2e}s, T0_S:{:.2e}s$".format(dataset["tau_0to0"], dataset["T0_0to0"])
+#        # end if
         legend.append(legend_str)
 
         lastTr = t_res[np.isfinite(np.array(dataset["fr"]))][-1]
@@ -507,10 +542,10 @@ def plot_fr_comparision(datasets,export_folder, plot_dataset=[]):
     plt.yscale("log")
     plt.grid(which='both')
 
-    plt.ylabel('Failure Rate (1/s)')
+    plt.ylabel('Failure Rate ($s^{-1}$)')
     plt.xlabel('Resolution time (ps)')
 
-    plt.yticks([10**i for i in range(-8, 5)])
+    plt.yticks([10**i for i in range(-4, 9)])
     plt.xticks(range(math.floor(int(t_res[0])/200)*200, int(lastTr)+50, 200) )
 
     plt.legend(legend)
