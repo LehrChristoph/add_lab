fixed T0 calculation

lab1/python/createFigures.py
@@ -61,7 +61,7 @@ def do_calculations(datasets):
 
         for keyPostfix in lateTransitionTypes:
             [tau,offset] = calculate_T0_tau(dataset["data"]["t_res"][t_res_min:t_res_max], dataset["data"][f"mtbu{keyPostfix}"][t_res_min:t_res_max])
-            T0 = (1/(offset*f_clk*lambda_dat*dataset["data"]["mtbu"][t_res_0]))
+            T0 = (1/(offset*f_clk*lambda_dat))
             dataset["data"][f"tau{keyPostfix}"] = tau
             dataset["data"][f"T0{keyPostfix}"] = T0
         # end for
@@ -72,7 +72,7 @@ def do_calculations(datasets):
                 T0 = np.nan
             else:
                 [tau,offset] = calculate_T0_tau(dataset["data"]["t_res"][t_res_glitch_min:], dataset["data"][f"mtbu{keyPostfix}"][t_res_glitch_min:])
-                T0 = (1/(offset*f_clk*lambda_dat*dataset["data"]["mtbu"][t_res_0]))
+                T0 = (1/(offset*f_clk*lambda_dat))
             # end if
             dataset["data"][f"tau{keyPostfix}"] = tau
             dataset["data"][f"T0{keyPostfix}"] = T0