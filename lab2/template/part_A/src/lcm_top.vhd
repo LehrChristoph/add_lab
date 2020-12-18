ee8177ec5bd5bc1ef7d4059ccb0be0f6bae50295
Julian Spitzer <spitzer.julian@gmail.com>
2020-12-18 13:25:04 +0100

    Simplify top-level entity

--- lab2/template/part_A/src/lcm_top.vhd
+++ lab2/template/part_A/src/lcm_top.vhd
@@ -20,41 +20,21 @@ entity LCM_top is
 end LCM_top;
 
 architecture STRUCTURE of LCM_top is
-
-constant DATA_WIDTH : Integer := 8;
-
-component lcm is	
-	generic ( DATA_WIDTH : natural);
-	port (
-		AB : in std_logic_vector ( DATA_WIDTH-1 downto 0 );
-		RESULT : out std_logic_vector ( DATA_WIDTH-1 downto 0 );
-		rst : in std_logic;
-		i_req:  in std_logic;
-		i_ack:  out std_logic;
-		o_req: out std_logic;
-		o_ack: in std_logic
-	);
-end component lcm;
-	
-	signal AB, result_tmp : std_logic_vector(DATA_WIDTH-1 downto 0);
-
+	constant DATA_WIDTH : Integer := result'length;
 begin
-	
-lcm_calc: component lcm
+
+lcm_calc: entity work.lcm
 	generic map ( DATA_WIDTH => DATA_WIDTH)
 	port map(
-		AB => AB,
-		RESULT => result_tmp,
+		AB => A&B,
+		RESULT => result,
 		rst => res_n,
 		i_req => req_AB,
 		i_ack => ack_AB,
 		o_req => req_result,
 		o_ack => ack_result
 	);
-	
-	AB(DATA_WIDTH   -1 downto DATA_WIDTH/2) <= A;
-	AB(DATA_WIDTH/2 -1 downto            0) <= B;
-	A_deb <= result_tmp(DATA_WIDTH   -1 downto DATA_WIDTH/2);
-	B_deb <= result_tmp(DATA_WIDTH/2 -1 downto            0);
-	result <= result_tmp;
+
+	A_deb <= A;
+	B_deb <= B;
 end STRUCTURE;
