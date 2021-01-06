6b4810159141da78d390f0ad263cc691d7ea3f14
Julian Spitzer <spitzer.julian@gmail.com>
2021-01-06 02:22:50 +0100

    Change synchronous design of part_B so first iteration is always performed and no intermediate results show on output

--- lab2/template/part_B/src/lcm.vhd
+++ lab2/template/part_B/src/lcm.vhd
@@ -5,8 +5,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.numeric_std.all;
 
-use work.utils_pkg.all;
-
 entity LCM is
     generic (
         DATA_WIDTH : natural := 16
@@ -14,7 +12,8 @@ entity LCM is
     port(
         clk: in std_logic;
         res_n: in std_logic;
-        AB : in std_logic_vector(DATA_WIDTH-1 downto 0);
+        A : in std_logic_vector(DATA_WIDTH/2-1 downto 0);
+        B : in std_logic_vector(DATA_WIDTH/2-1 downto 0);
         ready : in std_logic;
         done : out std_logic;
         result: out std_logic_vector(DATA_WIDTH-1 downto 0);
@@ -28,31 +27,35 @@ architecture beh of LCM is
 
     -- Define registers
     type TYPE_REGS is record
-        fsm_state : TYPE_FSM_STATE;
-        valid     : std_logic;
-        done      : std_logic;
-        A         : unsigned(DATA_WIDTH/2-1 downto 0);
-        B         : unsigned(DATA_WIDTH/2-1 downto 0);
-        sumA      : unsigned(DATA_WIDTH-1 downto 0);
-        sumB      : unsigned(DATA_WIDTH-1 downto 0);
+        fsm_state  : TYPE_FSM_STATE;
+        valid      : std_logic;
+        done       : std_logic;
+        first_loop : std_logic;
+        A          : unsigned(DATA_WIDTH/2-1 downto 0);
+        B          : unsigned(DATA_WIDTH/2-1 downto 0);
+        sumA       : unsigned(DATA_WIDTH-1 downto 0);
+        sumB       : unsigned(DATA_WIDTH-1 downto 0);
+        result     : std_logic_vector(DATA_WIDTH-1 downto 0);
     end record;
 
     -- Reset values of registers
     constant CONST_REGS_RESET : TYPE_REGS := (
-        fsm_state => IDLE,
-        valid     => '0',
-        done      => '0',
-        A         => (others => '0'),
-        B         => (others => '0'),
-        sumA      => (others => '0'),
-        sumB      => (others => '0')
+        fsm_state  => IDLE,
+        valid      => '0',
+        done       => '0',
+        first_loop => '0',
+        A          => (others => '0'),
+        B          => (others => '0'),
+        sumA       => (others => '0'),
+        sumB       => (others => '0'),
+        result     => (others => '0')
     );
 
     signal R : TYPE_REGS;
 begin
     done <= R.done;
     valid <= R.valid;
-    result <= std_logic_vector(R.sumA);
+    result <= R.result;
 
     lcm : process(clk, res_n)
         variable S : TYPE_REGS;
@@ -63,13 +66,15 @@ begin
             S := R;
             S.done  := '0';
             S.valid := '0';
+            S.first_loop := '0';
             case R.fsm_state is
                 when IDLE =>
                     if ready = '1' then
                         S.fsm_state := CALC;
                         S.done := '1';
-                        S.A := unsigned(AB(DATA_WIDTH-1 downto DATA_WIDTH/2));
-                        S.B := unsigned(AB(DATA_WIDTH/2-1 downto 0));
+                        S.first_loop := '1';
+                        S.A := unsigned(A);
+                        S.B := unsigned(B);
                         S.sumA := resize(S.A, S.sumA'length);
                         S.sumB := resize(S.B, S.sumB'length);
                     end if;
@@ -80,10 +85,10 @@ begin
                         S.sumB := R.sumB + resize(R.B, R.sumB'length);
                     end if;
 
-                    -- using R instead of S delays the output by 1 cycle but shortens the critical path
-                    if R.sumA = R.sumB then
+                    if R.first_loop = '0' and R.sumA = R.sumB then
                         S.fsm_state := IDLE;
                         S.valid := '1';
+                        S.result := std_logic_vector(R.sumA);
                     end if;
             end case;
 
