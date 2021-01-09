aba5f0e930a6cbfc4049b3dde25129bcae37390f
LehrChristoph <christoph.lehr@gmx.at>
2021-01-09 13:32:57 +0100

    fixed compile errors of c_element and completion detector

--- lab2/template/part_G/src/Async-NCL-Library/components/c_element.vhd
+++ lab2/template/part_G/src/Async-NCL-Library/components/c_element.vhd
@@ -15,15 +15,31 @@ entity c_element is
 end c_element;
 
 architecture STRUCTURE of c_element is
-    
+	signal set,reset, trigger : std_logic;
+	signal init_output, temp_output : std_logic;
 begin
-	c_elem_proc : process(in1, in2)
+
+	set <= '1' when ((in1 ='1') and (in2 = '1')) 	else '0';
+	reset <= '1' when ((in1 ='0') and (in2 = '0')) 	else '0'; 
+	init_output <= '1' when ( out1 /='1' and out1 /='0') 	else '0'; 
+
+	c_elem_proc : process(set, reset, init_output, in1, in2)
 		variable prev_out : std_logic := '0';
 	begin
-		if (in1 = in2 ) then
-			prev_out := in1;
+		if reset = '1' then 
+			temp_output <= '0' after C_ELEMENT_2_DELAY;
+		elsif init_output = '1' then
+			if (in1 ='1') and (in2 = '1') then
+				temp_output <= '1' after C_ELEMENT_2_DELAY;
+			else 
+				temp_output <= '0' after C_ELEMENT_2_DELAY;
+			end if;
+		elsif rising_edge(set) then
+			temp_output <= '1';
 		end if;
-		out1 <= prev_out after C_ELEMENT_2_DELAY;
+
 	end process c_elem_proc;
 	
+	out1 <= temp_output after C_ELEMENT_2_DELAY;
+
 end architecture;
--- lab2/template/part_G/src/Async-NCL-Library/components/c_element_3in.vhd
+++ lab2/template/part_G/src/Async-NCL-Library/components/c_element_3in.vhd
@@ -16,15 +16,30 @@ entity c_element_3in is
 end c_element_3in;
 
 architecture STRUCTURE of c_element_3in is
-    
+	signal set,reset, trigger : std_logic;
+	signal init_output, temp_output : std_logic;  
 begin
-	c_elem_proc : process(in1, in2, in3)
+	
+	set <= '1' when ((in1 ='1') and (in2 = '1') and (in3 = '1')) 	else '0';
+	reset <= '1' when ((in1 ='0') and (in2 = '0') and (in3 = '0')) else '0'; 
+	init_output <= '1' when ( out1 /='1' and out1 /='0' and set= '0') 	else '0'; 
+	
+	c_elem_proc : process(set, reset, init_output, in1, in2, in3)
 		variable prev_out : std_logic := '0';
 	begin
-		if (in1 = in2) and (in1 = in3) then
-			prev_out := in1;
+		if reset = '1'  then 
+			temp_output <= '0';
+		elsif init_output = '1' then
+			if (in1 ='1') and (in2 = '1') and (in3 = '1') then
+				temp_output <= '1';
+			else 
+				temp_output <= '0';
+			end if;
+		elsif rising_edge(set) then
+			temp_output <= '1';
 		end if;
-		out1 <= prev_out  after C_ELEMENT_3_DELAY;
+
 	end process c_elem_proc;
 	
+	out1 <= temp_output after C_ELEMENT_3_DELAY;	
 end architecture;
