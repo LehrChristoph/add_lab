7279e4b2be418dbf269ad48e22b3889fef5ff878
Christoph Lehr <christoph.lehr@gmx.at>
2021-01-16 20:39:04 +0100

    implemented reset behavior

--- lab2/template/part_G/src/Async-NCL-Library/funcblocks/sel_a_larger_b.vhd
+++ lab2/template/part_G/src/Async-NCL-Library/funcblocks/sel_a_larger_b.vhd
@@ -25,29 +25,53 @@ entity sel_a_larger_b is
 	end sel_a_larger_b;
 
 architecture Behavioral of sel_a_larger_b is
-
+	signal enable_c_elements : std_logic;
+	signal in_selected_t, in_selected_f : std_logic_vector(DATA_WIDTH-1 downto 0);
+	signal in_buffered_t, in_buffered_f : std_logic_vector(DATA_WIDTH-1 downto 0);
+		
 	signal a_t, a_f : std_logic_vector(DATA_WIDTH/2 -1 downto 0);
 	signal b_t, b_f : std_logic_vector(DATA_WIDTH/2 -1 downto 0);
 	
 	signal a_larger_b, a_smaller_equal_b, a_smaller_b : std_logic_vector(DATA_WIDTH/2 -1 downto 0);
 	signal a_larger_b_current, a_smaller_equal_b_current, a_smaller_b_current : std_logic_vector(DATA_WIDTH/2 -2 downto 0);
 	signal a_larger_b_forward, a_smaller_equal_b_forward, a_smaller_b_forward : std_logic_vector(DATA_WIDTH/2 -2 downto 0);
-
-	signal stage_complete : std_logic_vector(DATA_WIDTH/2 -2 downto 0);
+	
+	signal stage_complete, stage_complete_current, stage_complete_forward, stage_complete_next : std_logic_vector(DATA_WIDTH/2 -2 downto 0);
 	signal completion_vector : std_logic_vector(DATA_WIDTH/2 -1 downto 0);
-	signal computation_complete : std_logic;
+	
+	signal temp_t, temp_f, internal_ack : std_logic;
+	
 begin
-	a_t <= in_data_t(DATA_WIDTH - 1 downto DATA_WIDTH/2);
-	b_t <= in_data_t(DATA_WIDTH/2 -1 downto 0);
 
-	a_f <= in_data_f(DATA_WIDTH - 1 downto DATA_WIDTH/2);
-	b_f <= in_data_f(DATA_WIDTH/2 -1 downto 0);
+	GEN_C_ELEMENT : for i in 0 to DATA_WIDTH-1 generate	
+		c_element_inst_t : entity work.c_element
+		port map
+		(
+			in1 => in_selected_t(i),
+			in2 => not enable_c_elements,
+			out1 => in_buffered_t(i)
+		);
+		
+		c_element_inst_f : entity work.c_element
+		port map
+		(
+			in1 => in_selected_f(i),
+			in2 => not enable_c_elements,
+			out1 => in_buffered_f(i)
+		);
+	end generate GEN_C_ELEMENT;
+	
+	a_t <= in_buffered_t(DATA_WIDTH - 1 downto DATA_WIDTH/2);
+	b_t <= in_buffered_t(DATA_WIDTH/2 -1 downto 0);
+
+	a_f <= in_buffered_f(DATA_WIDTH - 1 downto DATA_WIDTH/2);
+	b_f <= in_buffered_f(DATA_WIDTH/2 -1 downto 0);
 	
 	a_larger_b(a_larger_b'length -1) <= a_t(a_t'length -1) and b_f(b_f'length -1) after AND2_DELAY;	
 	a_smaller_equal_b(a_smaller_equal_b'length -1) <= a_f(a_f'length -1)  or b_t(b_t'length -1) after OR2_DELAY;
 	a_smaller_b(a_smaller_b'length -1) <= a_f(a_f'length -1) and b_t(b_t'length -1) after AND2_DELAY;	
 	
-	completion_vector(a_smaller_b'length -1) <= a_larger_b(a_larger_b'length -1) or a_smaller_equal_b(a_smaller_equal_b'length -1) after OR2_DELAY;
+	completion_vector(a_smaller_b'length -1) <= a_larger_b(a_larger_b'length -1) xor a_smaller_equal_b(a_smaller_equal_b'length -1) after OR2_DELAY;
 	
 	GEN_a_larger_b : for i in a_larger_b'length -2 downto 0 generate
 	
@@ -63,9 +87,8 @@ begin
 		a_smaller_equal_b(i) <= a_smaller_equal_b_forward(i) or a_smaller_b(i+1) after OR2_DELAY;
 		a_smaller_b(i) <= a_smaller_b_forward(i) or a_smaller_b(i+1) after OR2_DELAY;
 		
-		stage_complete(i) <= a_larger_b(i) or a_smaller_equal_b(i) after OR2_DELAY;
-	
-		c_element_stage_complete : entity work.c_element
+		stage_complete(i) <= a_larger_b(i) xor a_smaller_equal_b(i) after OR2_DELAY;
+		c_element_stage_complete_forward : entity work.c_element
 		port map
 		(
 			in1 => stage_complete(i),
@@ -80,7 +103,7 @@ begin
 	(
 		in1 => a_larger_b(0),
 		in2 => completion_vector(0),
-		out1 => selector_t
+		out1 => temp_t
 	);
 	
 	c_element_inst_selector_f : entity work.c_element
@@ -88,7 +111,7 @@ begin
 	(
 		in1 => a_smaller_equal_b(0),
 		in2 => completion_vector(0),
-		out1 => selector_f
+		out1 => temp_f
 	);
 		
 	c_element_inst_ack : entity work.c_element
@@ -96,7 +119,31 @@ begin
 	(
 		in1 => completion_vector(0),
 		in2 => ack_in,
-		out1 => ack_out
+		out1 => internal_ack
 	);
+	
+	reset_function : process (temp_t, temp_f, rst, internal_ack, in_data_t, in_data_f)
+	begin
+		if rst = '1' then
+			selector_t <= '0';
+			selector_f <= '0';
+		
+			in_selected_t <= (others => '0');
+			in_selected_f <= (others => '0');
+			
+			ack_out <= '0';
+			enable_c_elements <= '1';
+		else
+			in_selected_t <= in_data_t;
+			in_selected_f <= in_data_f;
+			
+			selector_t <= temp_t;
+			selector_f <= temp_f;
+			
+			ack_out <= internal_ack;
+			enable_c_elements <= internal_ack;
+
+		end if;
+	end process;
 	
 end Behavioral;
--- lab2/template/part_G/src/Async-NCL-Library/funcblocks/sel_a_not_b.vhd
+++ lab2/template/part_G/src/Async-NCL-Library/funcblocks/sel_a_not_b.vhd
@@ -25,21 +25,49 @@ entity sel_a_not_b is
 	end sel_a_not_b;
 
 architecture Behavioral of sel_a_not_b is
+	signal enable_c_elements : std_logic;
+	signal in_selected_t, in_selected_f : std_logic_vector(DATA_WIDTH-1 downto 0);
+	signal in_buffered_t, in_buffered_f : std_logic_vector(DATA_WIDTH-1 downto 0);
+	
 	signal selector_complete : std_logic;
 	signal a_t, a_f, b_t, b_f : std_logic_vector(DATA_WIDTH/2 -1 downto 0);
 	signal a_t_xor_b_t, a_f_xor_b_f, a_t_xor_b_f, a_f_xor_b_t, a_not_b_t, a_not_b_f :std_logic_vector(DATA_WIDTH/2 -1 downto 0);
 	
+	signal temp_t, temp_f, internal_ack : std_logic;
+	
 	signal stage_complete : std_logic_vector(DATA_WIDTH/2 -1 downto 1);
 	signal completion_vector : std_logic_vector(DATA_WIDTH/2 -1 downto 0);
-	signal computation_complete : std_logic;
 	
+	attribute keep: boolean;
+	attribute keep of a_t, a_f, b_t, b_f: signal is true;
+	attribute keep of stage_complete, completion_vector : signal is true;
+	attribute keep of in_buffered_t, in_buffered_f: signal is true;
+
 begin
+	
+	GEN_C_ELEMENT : for i in 0 to DATA_WIDTH-1 generate	
+		c_element_inst_t : entity work.c_element
+		port map
+		(
+			in1 => in_selected_t(i),
+			in2 => not enable_c_elements,
+			out1 => in_buffered_t(i)
+		);
+		
+		c_element_inst_f : entity work.c_element
+		port map
+		(
+			in1 => in_selected_f(i),
+			in2 => not enable_c_elements,
+			out1 => in_buffered_f(i)
+		);
+	end generate GEN_C_ELEMENT;
+	
+	a_t <= in_buffered_t(DATA_WIDTH - 1 downto DATA_WIDTH/2);
+	b_t <= in_buffered_t(DATA_WIDTH/2 -1 downto 0);
 
-	a_t <= in_data_t(DATA_WIDTH - 1 downto DATA_WIDTH/2);
-	b_t <= in_data_t(DATA_WIDTH/2 -1 downto 0);
-
-	a_f <= in_data_f(DATA_WIDTH - 1 downto DATA_WIDTH/2);
-	b_f <= in_data_f(DATA_WIDTH/2 -1 downto 0);
+	a_f <= in_buffered_f(DATA_WIDTH - 1 downto DATA_WIDTH/2);
+	b_f <= in_buffered_f(DATA_WIDTH/2 -1 downto 0);
 		
 	a_t_xor_b_t(0) <= a_t(0) xor b_t(0) after XOR_DELAY;
 	a_f_xor_b_f(0) <= a_f(0) xor b_f(0) after XOR_DELAY;
@@ -50,7 +78,7 @@ begin
 	a_not_b_t(0) <= a_t_xor_b_t(0) and a_f_xor_b_f(0) after AND2_DELAY;
 	a_not_b_f(0) <= a_t_xor_b_f(0) and a_f_xor_b_t(0) after AND2_DELAY;
 	
-	completion_vector(0) <= a_not_b_t(0) or a_not_b_f(0) after OR2_DELAY;
+	completion_vector(0) <= a_not_b_t(0) xor a_not_b_f(0) after OR2_DELAY;
 	
 	
 	GEN_A_NOT_B : for i in 1 to DATA_WIDTH/2 -1 generate
@@ -61,9 +89,9 @@ begin
 		a_f_xor_b_t(i) <= a_f(i) xor b_t(i) after XOR_DELAY;
 				
 		a_not_b_t(i) <= (a_t_xor_b_t(i) and a_f_xor_b_f(i))  or a_not_b_t(i-1) after AND2_DELAY+OR2_DELAY;
-		a_not_b_f(i) <=  a_t_xor_b_f(i) and a_f_xor_b_t(i)  and a_not_b_f(i-1) after AND3_DELAY;
+		a_not_b_f(i) <= (a_t_xor_b_f(i) and a_f_xor_b_t(i)) and a_not_b_f(i-1) after AND3_DELAY;
 		
-		stage_complete(i) <= a_not_b_t(i) or a_not_b_f(i) after OR2_DELAY;
+		stage_complete(i) <= a_not_b_t(i) xor a_not_b_f(i) after OR2_DELAY;
 		
 		c_element_stage_complete : entity work.c_element
 		port map
@@ -80,7 +108,7 @@ begin
 	(
 		in1 => a_not_b_t(a_not_b_t'length -1),
 		in2 => completion_vector(completion_vector'length -1),
-		out1 => selector_t
+		out1 => temp_t
 	);
 	
 	c_element_inst_selector_f : entity work.c_element
@@ -88,7 +116,7 @@ begin
 	(
 		in1 => a_not_b_f(a_not_b_f'length -1),
 		in2 => completion_vector(completion_vector'length -1),
-		out1 => selector_f
+		out1 => temp_f
 	);
 		
 	c_element_inst_ack : entity work.c_element
@@ -96,12 +124,32 @@ begin
 	(
 		in1 => completion_vector(completion_vector'length -1),
 		in2 => ack_in,
-		out1 => ack_out
+		out1 => internal_ack
 	);
 	
---	selector_t <= a_not_b_t(a_not_b_t'length -1);
---	selector_f <= a_not_b_f(a_not_b_f'length -1);
---	
---	selector_complete <= selector_t or selector_f after OR2_DELAY;
+	
+	reset_function : process (temp_t, temp_f, rst, internal_ack, in_data_t, in_data_f)
+	begin
+		if rst = '1' then
+			selector_t <= '0';
+			selector_f <= '0';
+		
+			in_selected_t <= (others => '0');
+			in_selected_f <= (others => '0');
+			
+			ack_out <= '0';
+			enable_c_elements <= '1';
+		else
+			in_selected_t <= in_data_t;
+			in_selected_f <= in_data_f;
+			
+			selector_t <= temp_t;
+			selector_f <= temp_f;
+			
+			ack_out <= internal_ack;
+			enable_c_elements <= internal_ack;
 
+		end if;
+	end process;
+	
 end Behavioral;
